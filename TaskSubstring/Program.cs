0cf8556415e3860f9ff4f523091f92a63884ef23
ZeynebR <zeynabnr@code.edu.az>
2023-10-11 08:06:46 +0200

    task added

---
+++ TaskSubstring/Program.cs
@@ -0,0 +1,17 @@
+﻿string Name = "Hello World";
+
+
+Console.WriteLine(substring(0,7));
+
+
+string substring(int startindex, int length)
+{
+    string final = "";
+
+	for (int i = startindex; i < startindex+length; i++)
+	{
+        final += Name[i];
+
+    }
+    return final;   
+} 
