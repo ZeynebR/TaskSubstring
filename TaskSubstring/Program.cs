78d9d43dd4f4fadb6e51ceadd21e780a30af9c40
ZeynebR <zeynabnr@code.edu.az>
2023-10-11 08:55:55 +0200

    Condition added

--- TaskSubstring/Program.cs
+++ TaskSubstring/Program.cs
@@ -1,17 +1,27 @@
-﻿string Name = "Hello World";
+﻿string Name = "Zeynab";
 
 
-Console.WriteLine(substring(0,7));
+Console.WriteLine(substring(0,4));
 
 
 string substring(int startindex, int length)
 {
     string final = "";
 
-	for (int i = startindex; i < startindex+length; i++)
-	{
-        final += Name[i];
+    for (int i = startindex; i < startindex + length; i++)
+    {
+        if (startindex + length < Name.Length)
+        {
+            final += Name[i];
+        }
+
 
     }
-    return final;   
+    if (startindex + length > Name.Length)
+    {
+        Console.WriteLine("Parameter length is longer than name length");
+    }
+
+    return final;
+    
 } 
